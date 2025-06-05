5bd2d48c07b490a6a180f184d62ca855910f1d4d
zaynab10o <zaynabm766@gmail.com>
2025-06-05 21:41:16 +0300

    added a test route

--- routes/api.php
+++ routes/api.php
@@ -383,3 +383,9 @@ Route::prefix('/admin')->middleware('auth.admin')->group(function () {
 
 
 });
+
+Route::get('/test', function (Request $request) {
+
+    return response()->json(['message' => 'Test route is working!']);
+
+});

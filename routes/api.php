9f35cc0db182ed571279b10ba83f250c67666c64
zaynab10o <zaynabm766@gmail.com>
2025-06-05 21:42:50 +0300

    removed the test route and fixed the working tree

--- routes/api.php
+++ routes/api.php
@@ -383,9 +383,3 @@ Route::prefix('/admin')->middleware('auth.admin')->group(function () {
 
 
 });
-
-Route::get('/test', function (Request $request) {
-
-    return response()->json(['message' => 'Test route is working!']);
-
-});
